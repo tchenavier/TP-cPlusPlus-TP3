e98e279e910e94de2d7a24f54f5fda90f7ded63f
tchenavier <thibaud.chenavier@gmail.com>
2025-11-03 15:26:30 +0100

    Ajoutez des fichiers projet.

---
+++ TP c++ cour.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    char str[1001];
+    char lettre = 'a';
+    int i = 0;
+    int l;
+    int n;
+
+    fgets(str, 1000, stdin);
+    l = strlen(str);
+    n = l;
+    do
+    {
+        scanf_s("%c", &lettre);
+
+    } while (lettre == ' '|| lettre == '<' || lettre == '>');
+    
+    while (str[n] != '\0'){
+        i++;
+        n--;
+        if (str[n] != lettre)
+        {
+            //cette ligne ne ser a rien, hormie écrire sa
+        }
+        else
+        {
+            break;
+        }
+    } 
+
+    if (i != l)
+    {
+        printf("Le caractere %c se trouve a la position %d a partir de la droite", lettre, i);
+    }
+    else if (i == l)
+    {
+        printf("Le caractere %c n'a pas ete trouve dans la chaine", lettre);
+    }
+    return 0;
+}
