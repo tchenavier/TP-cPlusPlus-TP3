637b77019515a9d0afa4497d1259e686fd7a8052
tchenavier <thibaud.chenavier@gmail.com>
2025-11-04 16:56:57 +0100

    ex4

---
+++ ex4.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char** argv)
+{
+    char str[1001];
+    int l, i = 0, d = 0;
+
+    fgets(str, 1000, stdin);
+    l = strlen(str);
+
+    for (i = 0; i < l; i++)
+    {
+        d = str[i] - 65;
+        if (0 <= d && d < 25)
+        {
+            str[i] = d + 97;
+        }
+    }
+    printf(" %s", str);
+}
