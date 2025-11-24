1a45ba45d329f5d23910f5b26384f14100e847b0
tchenavier <thibaud.chenavier@gmail.com>
2025-11-24 15:08:19 +0100

    horloge pour avoir la durée

--- TP4Ex1.cpp
+++ TP4Ex1.cpp
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <chrono>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+
+#define NB_VAL 300000
 
 int main(int argc, char** argv)
 {
-
-    long int tab[3000];//Remplacer 3000 par 300000
+    long int tab[NB_VAL];//Remplacer 3000 par 300000
     int chiffre,j, i=0,index=0,n=0,min;
    
-    //printf("salut");
-    for (; i < 3000;i++) {// Remplacer 3000 par 300000
-        scanf_s("%d", &chiffre);
-        tab[i] = chiffre;
-        if (chiffre == -1)
-        {
-            break;
-        }
+    srand(time(NULL));//horloge
+
+    using clock_type = std::chrono::high_resolution_clock;//horloge
+    auto start = clock_type::now();//horloge
+
+    for (; i < NB_VAL;i++) {// Remplacer 3000 par 300000
+        
+        tab[i] = rand() % 300000;
+        
     }
     n = i;
-    chiffre= n;
-        for (i = 0; i < n - 1; i++) {//tris par selection
+    chiffre= i;
+        for (i = 0; i < n - 1; i++) {//tris par selection passer au tris rapide (complexiter logaritmique moins d'opération)
             index = i;
             for (j = i + 1; j < n ; j++) { // utilisation de j pour avoir deux avancement
                 if (tab[index] > tab[j]) {
@@ -31,10 +37,15 @@ int main(int argc, char** argv)
                 tab[index] = min;
             }
         }
-        for (i = 0; i < chiffre;i++) {//affichage
-                printf("%ld \n",tab[i]);
-        }    
 
+        auto last = clock_type::now();//horloge
+        auto interval = last - start;//horloge
+
+        std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(interval).count() << "ms" << std::endl; //horloge
+
+       /*for (i = 0; i < chiffre; i++) {//affichage
+                printf("%ld \n",tab[i]);
+       }*/
     return 0;
 }
 //https://fr.wikipedia.org/wiki/Tri_%C3%A0_bulles
