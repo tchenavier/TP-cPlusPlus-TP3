72e5aefa8138921b82681404b699e81b434fad99
tchenavier <thibaud.chenavier@gmail.com>
2025-11-25 15:26:31 +0100

    Fin TP5

--- TP5Ex1.cpp
+++ TP5Ex1.cpp
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <cmath>
 
-int valeurAbsolue(int valeur)
+int xPuissY(int x, int y)
 {
-    if (valeur < 0)
+    int result = 0;
+    result = pow(x, y);
+
+    return result;
+}//pour l'exercise 1 on na besoin que de sa
+
+int divise(int Un, int Deux)
+{
+    int resulta;
+
+    if (Deux == 0)
     {
-        valeur = -1 * (valeur);
+        return -1;
     }
     else
     {
-
+        resulta = Un / Deux;
+        return resulta;
     }
-    return valeur;
-}//pour l'exercise 1 on na besoin que de sa
-
+}//pour l'exercise 10 on na besoin que de sa
 
 int main(int argc, char** argv)
 {
     int val;
+    int val2;
+    int retour;
 
     scanf_s("%d", &val);
-    val = valeurAbsolue(val);
+    scanf_s("%d", &val2);
+
+    //val = xPuissY(val,val2);
+    retour = divise(val, val2);
 
     printf("%d",val);
 
